3e25a08d6f70b9da01154a76340336d786af72d9
lindsay <932947839@qq.com>
2019-03-18 21:33:21 +0800

    个人信息页

--- pages/suoda/sd.js
+++ pages/suoda/sd.js
@@ -5,79 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-    lists: [
-      "学院", "图书馆", "校医院", "教学楼", "食堂", "浴室", "信息大厅", "操场", "保卫处", "学生处"
-    ],
-    indexId: 0,
+    // onPullDownRefresh: function () {
+    //   wx.stopPullDownRefresh()
+    // },
+    myinfo: null
+
   },
-  // 左侧点击事件
-  jumpIndex(e) {
-    let index = e.currentTarget.dataset.menuindex
-    let that = this
-    that.setData({
-      indexId: index
-    });
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this
-    wx.getSystemInfo({
+    var stu = wx.getStorageSync('student');
+    this.setData({ myinfo: stu });
+    // console.log(this.data.myinfo);
+  },
+  exit: function (e) {
+    wx.showModal({
+      title: '提示',
+      content: '是否确认退出',
       success: function (res) {
-        that.setData({
-          winHeight: res.windowHeight
-        });
+        if (res.confirm) {
+          // console.log('用户点击确定')
+          wx.removeStorageSync('student');
+          //页面跳转
+          wx.redirectTo({
+            url: '../login/login',
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
       }
-    });
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  resetpwd: function (e) {
+    var no = this.data.myinfo.no;
+    wx.navigateTo({
+      url: '../password/password?no=' + no,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  setemail: function (e) {
+    var no = this.data.myinfo.no;
+    wx.navigateTo({
+      url: '../email/email?no=' + no,
+    })
   }
 })
