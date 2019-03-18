3a0792d526ba34ccc8c2a1df94845e3d806ca249
liyifeng <787339520@qq.com>
2019-03-19 00:05:38 +0800

    修复主页滑动bug，添加个人页面滑动

--- pages/liyifeng/lyf.js
+++ pages/liyifeng/lyf.js
@@ -1,4 +1,8 @@
 // pages/liyifeng/lyf.js
+var time = 0;
+var touchDot = 0;//触摸时的原点
+var interval = "";
+var flag_hd = true;
 Page({
 
   /**
@@ -27,6 +31,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
     // 查看是否授权
     // wx.getSetting({
     //   success: function (res) {
@@ -55,6 +60,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    flag_hd = true;    //重新进入页面之后，可以再次执行滑动切换页面代码
+    clearInterval(interval); // 清除setInterval
+    time = 0;
 
   },
 
@@ -91,6 +99,37 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+  // 触摸结束事件
+  touchEnd: function (e) {
+    var touchMove = e.changedTouches[0].pageX;
+    // 向左滑动   
+    // if (touchMove - touchDot <= -40 && time < 10 && flag_hd == true) {
+    //   flag_hd = false;
+    //   //执行切换页面的方法
+    //   console.log("向右滑动");
+    //   wx.switchTab({
+    //     url: '../contact/contact'
+    //   })
+    // }
+    // 向右滑动   
+    if (touchMove - touchDot >= 40 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      //执行切换页面的方法
+      console.log("向左滑动");
+      wx.switchTab({
+        url: '../product/product'
+      })
+    }
+    clearInterval(interval); // 清除setInterval
+    time = 0;
   },
 
 
